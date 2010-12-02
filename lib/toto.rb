52bd66f14cd3e32dd6a0a09523dd957b91d79ae1
cloudhead <self@cloudhead.net>
2010-12-01 21:32:21 -0500

    2nd attempt at ruby 1.8 fix. No I don't have ruby 1.8 right now.

--- lib/toto.rb
+++ lib/toto.rb
@@ -97,7 +97,7 @@ module Toto
       self[:root]
     end
 
-    def go route, type = :html, env = {}
+    def go route, env = {}, type = :html
       route << self./ if route.empty?
       type, path = type =~ /html|xml|json/ ? type.to_sym : :html, route.join('/')
       context = lambda do |data, page|
@@ -335,7 +335,7 @@ module Toto
       path, mime = @request.path_info.split('.')
       route = (path || '/').split('/').reject {|i| i.empty? }
 
-      response = @site.go(route, *(mime ? mime : []), env)
+      response = @site.go(route, env, *(mime ? mime : []))
 
       @response.body = [response[:body]]
       @response['Content-Length'] = response[:body].length.to_s unless response[:body].empty?
