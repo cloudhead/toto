6403367d4598e3964b8806484cd6f83b5883b4d8
Aleksey V Zapparov <ixti@member.fsf.org>
2011-08-07 21:16:21 +0200

    Added tests

--- test/toto_test.rb
+++ test/toto_test.rb
@@ -280,6 +280,31 @@ context Toto do
   context "extensions to the core Ruby library" do
     should("respond to iso8601") { Date.today }.respond_to?(:iso8601)
   end
+
+  context "on non-development environment" do
+    Toto.env = 'production'
+
+    context "GET /" do
+      setup { @toto.get('/') }
+      should("do not include ~DRAFT~ articles") { topic.body }.not_includes_html("li" => /~DRAFT~/)
+    end
+
+    context "GET a ~DRAFT~ article" do
+      setup { @toto.get("/2011/08/07/the-beautiful-people") }
+      asserts("returns a 200")                { topic.status }.equals 200
+      asserts("content type is set properly") { topic.content_type }.equals "text/html"
+      should("contain the article")           { topic.body }.includes_html("h2" => /~DRAFT~/)
+    end
+  end
+
+  context "on development environment" do
+    Toto.env = 'development'
+
+    context "GET /" do
+      setup { @toto.get('/') }
+      should("include ~DRAFT~ articles") { topic.body }.includes_html("li" => /~DRAFT~/)
+    end
+  end
 end
 
 
